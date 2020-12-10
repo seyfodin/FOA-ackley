d7c8b601b5d61e3b7f2ecc665bd2bdcb5c801ea4
seyfodin.a <seyfodin.a@gmail.com>
2020-12-10 16:28:47 +0330

    final change

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 from ackley import Ackley
 from foa import FOA
 
-fun=Ackley(2,-5,5,0.25,'contour')
-foa=FOA(fun=fun,numSedds=30, maxTime=100, lifeTime=2, areaLimit=30, localMotion=0.1, localSeeding=4,transferRate=4, globalSeeding=2)
-foa.run()
-
-#foa=FOA(fun=Ackley(2,-5,5,0.25,'3d'),numSedds=30, maxTime=1000, lifeTime=2, areaLimit=30, localMotion=0.1, localSeeding=4,transferRate=4, globalSeeding=2)
+#foa=FOA(fun=Ackley(2,-5,5,0.05,'contour'),numSedds=30, maxTime=100, lifeTime=2, areaLimit=30, localMotion=0.1, localSeeding=4,transferRate=4, globalSeeding=2)
 #foa.run()
 
-#foa=FOA(fun=Ackley(1,-5,5,0.25,'2d'),numSedds=30, maxTime=1000, lifeTime=2, areaLimit=30, localMotion=0.01, localSeeding=4,transferRate=3, globalSeeding=2)
+foa=FOA(fun=Ackley(2,-5,5,0.05,'3d'),numSedds=30, maxTime=100, lifeTime=2, areaLimit=30, localMotion=0.1, localSeeding=4,transferRate=4, globalSeeding=2)
+foa.run()
+
+#foa=FOA(fun=Ackley(1,-5,5,0.05,'2d'),numSedds=30, maxTime=100, lifeTime=2, areaLimit=30, localMotion=0.01, localSeeding=4,transferRate=3, globalSeeding=2)
 #foa.run()
